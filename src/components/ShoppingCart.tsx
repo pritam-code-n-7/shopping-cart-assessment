dee5803ba82c85ccc3dba0d018c50861bcc7a216
pritam-code-n-7 <pritamimca1003@gmail.com>
2024-06-09 15:58:27 +0530

    I add filter product by selected categories functionality; I add a dropdown menu for that in the ui

--- src/components/ShoppingCart.tsx
+++ src/components/ShoppingCart.tsx
@@ -6,8 +6,9 @@ interface ProductTypes {
   title: string;
   price: number;
   image: string;
+  category: string;
 }
-//Product list Parent
+//Product list 
 const ProductConst: React.FC<{ product: ProductTypes }> = ({ product }) => {
   return (
     <div key={product.id} className="grid grid-cols-3 border p-4">
@@ -18,30 +19,76 @@ const ProductConst: React.FC<{ product: ProductTypes }> = ({ product }) => {
   );
 };
 
-//main function
+//fetching product list
 function ShoppingCart() {
-
-  //declare state for storing products
+  
   const [products, setProducts] = useState<ProductTypes[]>([]);
+  const [categories, setCategories] = useState<string[]>([]);
+  const [selectedCategory, setSelectedCategory] = useState<string>("");
 
   //useEffect hooks that run once after component mounts
   useEffect(() => {
-    fetch("https://fakestoreapi.com/products") //used promise chain for managing asynchronous operation with fetch method
-      .then((res) => res.json())
-      .then((data) => setProducts(data))
-      .catch((error) => console.error("Error fetching products:", error));
+    const fetchData = async () => {
+      try {
+        const [productRes, categoryRes] = await Promise.all([
+          fetch("https://fakestoreapi.com/products"),
+          fetch("https://fakestoreapi.com/products/categories"),
+        ]);
+        const products = await productRes.json();
+        const categories = await categoryRes.json();
+
+        setProducts(products);
+        setCategories(categories);
+      } 
+      catch (error) {
+        console.error("Error fetching data", error);
+      }
+    };
+
+    fetchData();
   }, []);
 
-//mapped product list with conditional rendering
+  //target the value comes from selecting an option
+  const handleCategory = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    const category = event.target.value;
+    setSelectedCategory(category);
+  };
+
+  //filter the products by selected category
+  const filterProducts = selectedCategory
+    ? products.filter((product) => product.category === selectedCategory)
+    : products;
+
+  
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 p-4">
-      {products.length > 0 ? (
-        products.map((product) => (
-          <ProductConst product={product} key={product.id} />
-        ))
-      ) : (
-        <div>Loading...</div>
-      )}
+    <div>
+      <div className="p-2">
+        <label htmlFor="category-select" className="block mb-2">
+          Filter by category:
+        </label>
+        <select
+          id="category-select"
+          value={selectedCategory}
+          onChange={handleCategory}
+          className="p-2 border"
+        >
+          <option value="">All</option>
+          {categories.map((category) => (
+            <option key={category} value={category}>
+              {category}
+            </option>
+          ))}
+        </select>
+      </div>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 p-4">
+        {filterProducts.length > 0 ? (
+          filterProducts.map((product) => (
+            <ProductConst product={product} key={product.id} />
+          ))
+        ) : (
+          <div>Loading...</div>
+        )}
+      </div>
     </div>
   );
 }
